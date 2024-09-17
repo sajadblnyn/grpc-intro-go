eb002617390b879ab781f5fa6b7f9d8f6a469584
sajadblnyn <sajadbalaniyan.sb@gmail.com>
2024-09-17 09:33:04 +0330

    feat: add grpc error handling

--- server/currency.go
+++ server/currency.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/sajadblnyn/grpc-intro-go/data"
 	"github.com/sajadblnyn/grpc-intro-go/protos/currency/protos/currency"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type CurrencyServer struct {
@@ -36,7 +38,13 @@ func (c *CurrencyServer) handleUpdateCurrencies() {
 				if err != nil {
 					fmt.Println(err)
 				}
-				err = k.Send(&currency.RateResponse{Rate: ra, Base: currency.Currencies(currency.Currencies_value[r.GetBase().String()]), Destination: currency.Currencies(currency.Currencies_value[r.GetDestination().String()])})
+				err = k.Send(&currency.StreamingRateResponse{
+					Message: &currency.StreamingRateResponse_RateResponse{
+						RateResponse: &currency.RateResponse{
+							Rate:        ra,
+							Base:        currency.Currencies(currency.Currencies_value[r.GetBase().String()]),
+							Destination: currency.Currencies(currency.Currencies_value[r.GetDestination().String()])}}})
+
 				if err != nil {
 					fmt.Println(err)
 				}
@@ -48,6 +56,15 @@ func (c *CurrencyServer) handleUpdateCurrencies() {
 }
 
 func (c *CurrencyServer) GetRate(cx context.Context, rq *currency.RateRequest) (*currency.RateResponse, error) {
+	if rq.Base.String() == rq.Destination.String() {
+		err := status.Newf(codes.InvalidArgument, "base and destination currency are the same base:%s dest:%s", rq.Base.String(), rq.Destination.String())
+		err, wd := err.WithDetails(rq)
+		if wd != nil {
+			return nil, wd
+		}
+		return nil, err.Err()
+	}
+
 	r, err := c.rates.GetRate(rq.GetBase().String(), rq.GetDestination().String())
 	if err != nil {
 		return nil, err
@@ -58,8 +75,9 @@ func (c *CurrencyServer) GetRate(cx context.Context, rq *currency.RateRequest) (
 }
 
 func (c *CurrencyServer) SubscribeRates(cs currency.Currency_SubscribeRatesServer) error {
-
+	var requestAlreadyExistsForClient bool = false
 	for {
+		requestAlreadyExistsForClient = false
 		rr, err := cs.Recv()
 		if err == io.EOF {
 			fmt.Println("connection has been closed by client", err)
@@ -70,14 +88,35 @@ func (c *CurrencyServer) SubscribeRates(cs currency.Currency_SubscribeRatesServe
 			return err
 		}
 		rrl, ok := c.subscriptions[cs]
+
 		if !ok {
+
 			rrl = []*currency.RateRequest{}
 		}
-		rrl = append(rrl, rr)
+		for _, v := range rrl {
 
-		c.subscriptions[cs] = rrl
+			if v.Base == rr.Base && v.Destination == rr.Destination {
+				requestAlreadyExistsForClient = true
+				grpcErr := status.New(codes.AlreadyExists, "this currency rate request is already exists for you")
+				grpcErr, err = grpcErr.WithDetails(rr)
+				if err != nil {
+					fmt.Println("error in set details for grpc error")
+					break
+				}
+				cs.Send(&currency.StreamingRateResponse{Message: &currency.StreamingRateResponse_Error{Error: grpcErr.Proto()}})
+				break
 
-		fmt.Printf("client sent this message:%v\n", rr.Base)
+			}
+
+		}
+		if !requestAlreadyExistsForClient {
+			rrl = append(rrl, rr)
+
+			c.subscriptions[cs] = rrl
+			fmt.Printf("client sent this request base:%s destination:%s", rr.Base, rr.Destination)
+
+		}
+
 	}
 
 	return nil
